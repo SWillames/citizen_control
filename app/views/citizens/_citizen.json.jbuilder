6b11083f25b8ff198b0eed595251f93f1e811691
SWillames <sergiowoc@gmail.com>
2021-04-26 19:03:34 -0300

    resource citizen created.

---
+++ app/views/citizens/_citizen.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! citizen, :id, :full_name,, :cpf,, :email,, :birth_date, :telephone, :photograph, :status, :created_at, :updated_at
+json.url citizen_url(citizen, format: :json)
---
+++ spec/factories/citizens.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :citizen do
+    full_name { "MyString" }
+    cpf { "MyString" }
+    email { "MyString" }
+    birth_date { "2021-04-26" }
+    telephone { "MyString" }
+    photograph { "MyString" }
+    status { "MyString" }
+  end
+end
